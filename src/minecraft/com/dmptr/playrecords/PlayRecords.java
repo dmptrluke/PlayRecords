c152735534b834c3ca6430b961a56e5fa8e0ad7f
Neer Sighted <neersighted@myopera.com>
2013-04-04 15:20:56 -0700

    Restructure (easier to add new records).

--- src/minecraft/com/dmptr/playrecords/PlayRecords.java
+++ src/minecraft/com/dmptr/playrecords/PlayRecords.java
@@ -1,12 +1,13 @@
 /**
  * PlayRecords
- * 
+ *
  * @author Luke Rogers
  * @author Neer Sighted
  */
 
 package com.dmptr.playrecords;
 
+import java.util.Map;
 import java.util.HashMap;
 
 import net.minecraft.item.Item;
@@ -48,19 +49,53 @@ public class PlayRecords {
 
     // Declare types.
     public static boolean recordsInDungeons, recordsCraftable;
-    public static int obsidianDiscID, fireRecordID, discordRecordID,
-            pirateRecordID, callmeRecordID;
+    public static int blankObsidianRecordID, callmeRecordID, discordRecordID,
+            fireRecordID, pirateRecordID;
 
     public static Item blankObsidianRecord;
     public static HashMap<String, Item> records = new HashMap();
 
+    private static HashMap[] recordInfo = { new HashMap() {
+        {
+            put("id", callmeRecordID);
+            put("name", "callme");
+            put("title", "Carly Rae Jephsen - Call Me Maybe (8bit)");
+            put("iconIndex", 1);
+        }
+    }, new HashMap() {
+        {
+            put("id", discordRecordID);
+            put("name", "discord");
+            put("title", "Bomb Squad - Discord (8bit/remix)");
+            put("iconIndex", 6);
+        }
+    }, new HashMap() {
+        {
+            put("id", fireRecordID);
+            put("name", "fire");
+            put("title", "Billy Joel - We Didn't Start The Fire");
+            put("iconIndex", 16);
+        }
+    }, new HashMap() {
+        {
+            put("id", pirateRecordID);
+            put("name", "pirate");
+            put("title", "Hans Zimmer - He's a Pirate");
+            put("iconIndex", 19);
+        }
+    }, };
+    private static final Item[] vanillaRecords = { Item.record11,
+            Item.record13, Item.recordBlocks, Item.recordCat, Item.recordChirp,
+            Item.recordFar, Item.recordMall, Item.recordMellohi,
+            Item.recordStal, Item.recordStrad, Item.recordWait, Item.recordWard };
+
     // Create a creative tab.
-    public static CreativeTabs tabDiscs = new CreativeTabs("tabDiscs") {
+    public static final CreativeTabs tabDiscs = new CreativeTabs("tabDiscs") {
         public ItemStack getIconItemStack() {
             return new ItemStack(blankObsidianRecord);
         };
     };
-    
+
     @PreInit
     public void preInit(FMLPreInitializationEvent event) {
         // Load the configuration file.
@@ -69,22 +104,22 @@ public class PlayRecords {
         config.load();
 
         // Load options.
-        Property recordsInDungeonsConfig = config.get(
-                Configuration.CATEGORY_GENERAL, "recordsInDungeons", true);
+        Property recordsInDungeonsConfig = config
+                .get(Configuration.CATEGORY_GENERAL, "recordsInDungeons", true);
         recordsInDungeonsConfig.comment = "generate records in dungeons";
         recordsInDungeons = recordsInDungeonsConfig.getBoolean(false);
 
-        Property recordsCraftableConfig = config.get(
-                Configuration.CATEGORY_GENERAL, "recordsCraftable", true);
+        Property recordsCraftableConfig = config
+                .get(Configuration.CATEGORY_GENERAL, "recordsCraftable", true);
         recordsCraftableConfig.comment = "enable crafting recipes for records";
         recordsCraftable = recordsCraftableConfig.getBoolean(false);
 
         // Load item IDs.
-        obsidianDiscID = config.getItem("obsidianDiscID", 22639).getInt();
-        callmeRecordID = config.getItem("callmeRecordID", 22640).getInt();
-        discordRecordID = config.getItem("discordRecordID", 22641).getInt();
-        fireRecordID = config.getItem("fireRecordID", 22642).getInt();
-        pirateRecordID = config.getItem("pirateRecordID", 22643).getInt();
+        blankObsidianRecordID = config.getItem("record.blank", 22639).getInt();
+        callmeRecordID = config.getItem("record.callme", 22640).getInt();
+        discordRecordID = config.getItem("record.discord", 22641).getInt();
+        fireRecordID = config.getItem("record.fire", 22642).getInt();
+        pirateRecordID = config.getItem("record.pirate", 22643).getInt();
 
         // Save the config.
         config.save();
@@ -92,67 +127,126 @@ public class PlayRecords {
 
     @Init
     public void load(FMLInitializationEvent event) { // Add items.
-        blankObsidianRecord = new ItemBlankObsidianRecord(obsidianDiscID);
+        blankObsidianRecord = new ItemBlankObsidianRecord(blankObsidianRecordID);
 
-        // Add items.
-        records.put("callme", new ItemObsidianRecord(callmeRecordID, "callme",
-                "FelixMoog - Call Me Maybe").setIconIndex(19));
-        records.put("discord", new ItemObsidianRecord(discordRecordID,
-                "discord", "FelixMoog - Discord (Remix)").setIconIndex(6));
-        records.put("fire", new ItemObsidianRecord(fireRecordID, "fire",
-                "FelixMoog - We Didn't Start The Fire").setIconIndex(1));
-        records.put("pirate", new ItemObsidianRecord(pirateRecordID, "pirate",
-                "FelixMoog - He's A Pirate").setIconIndex(16));
+        // Loop over record info and create all the records.
+        for (HashMap info : recordInfo) {
+            records.put(info.get("name").toString(),
+                    new ItemObsidianRecord(Integer.parseInt(info.get("id")
+                            .toString()), info.get("name").toString(), info
+                            .get("title").toString()).setIconIndex(Integer
+                            .parseInt(info.get("id").toString())));
+        }
+        ;
 
         // Add vanilla records to creative tab.
-        Item.record11.setCreativeTab(tabDiscs);
-        Item.record13.setCreativeTab(tabDiscs);
-        Item.recordBlocks.setCreativeTab(tabDiscs);
-        Item.recordCat.setCreativeTab(tabDiscs);
-        Item.recordChirp.setCreativeTab(tabDiscs);
-        Item.recordFar.setCreativeTab(tabDiscs);
-        Item.recordMall.setCreativeTab(tabDiscs);
-        Item.recordMellohi.setCreativeTab(tabDiscs);
-        Item.recordStal.setCreativeTab(tabDiscs);
-        Item.recordStrad.setCreativeTab(tabDiscs);
-        Item.recordWait.setCreativeTab(tabDiscs);
-        Item.recordWard.setCreativeTab(tabDiscs);
-
-        // Check if record crafting is enabled.
-        if (recordsCraftable) {
-            // Make blank discs craftable.
-            ItemStack obsidianStack = new ItemStack(Block.obsidian);
-            ItemStack blockGoldStack = new ItemStack(Block.blockGold);
-
-            GameRegistry.addRecipe(new ItemStack(blankObsidianRecord), "xxx",
-                    "xyx", "xxx", 'x', obsidianStack, 'y', blockGoldStack);
-
-            // Add record crafting.
-            ItemStack blankObsidianRecordStack = new ItemStack(
-                    blankObsidianRecord);
-            ItemStack fireballChargeStack = new ItemStack(Item.fireballCharge);
-            ItemStack swordSteelStack = new ItemStack(Item.swordSteel);
-            ItemStack rawFishStack = new ItemStack(Item.fishRaw);
-            ItemStack boatStack = new ItemStack(Item.boat);
-
-            GameRegistry.addRecipe(new ItemStack(records.get("fire")), " x ",
-                    "xox", " x ", 'o', blankObsidianRecordStack, 'x',
-                    fireballChargeStack);
-            GameRegistry.addRecipe(new ItemStack(records.get("pirate")), " x ",
-                    "fof", " b ", 'o', blankObsidianRecordStack, 'x',
-                    swordSteelStack, 'f', rawFishStack, 'b', boatStack);
-        }
+        this.setVanillaRecordsTab();
 
         // Check if dungeon generation is enabled.
         if (recordsInDungeons) {
-            for (Item value : records.values()) {
-                ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST).addItem(
-                        new WeightedRandomChestContent(new ItemStack(value), 1,
-                                1, 5));
-            }
+            // Set up chest generation.
+            this.setupLoot();
         }
 
-        // Add item names to Language Registry.
+        // Check if record crafting is enabled.
+        if (recordsCraftable) {
+            // Set up record crafting.
+            this.setupCrafting();
+        }
+
+        // Set up renderers.
+        proxy.registerRenderers();
+
+        // Set up localizations.
+        this.setupLocalizations();
+
+    }
+
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        // stub
+    }
+
+    /**
+     * Helper to change vanilla records to use our creative tab.
+     * 
+     * @return nothing
+     * 
+     * @author Neer Sighted
+     */
+    public static void setVanillaRecordsTab() {
+        for (Item record : vanillaRecords) {
+            record.setCreativeTab(tabDiscs);
+        }
+    }
+
+    /**
+     * Helper to set up dungeon chest generation.
+     * 
+     * @return nothing
+     * 
+     * @author Luke Rogers
+     */
+    public static void setupLoot() {
+        for (Item record : records.values()) {
+            ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST)
+                    .addItem(new WeightedRandomChestContent(new ItemStack(
+                            record), 1, 1, 5));
+        }
+    }
+
+    /**
+     * Helper to set up record crafting.
+     * 
+     * @return nothing
+     * 
+     * @author Luke Rogers
+     */
+    public static void setupCrafting() {
+        // Make blank discs craftable.
+        GameRegistry.addRecipe(new ItemStack(blankObsidianRecord),
+                "ooo",
+                "ogo",
+                "ooo",
+                'o',
+                new ItemStack(Block.obsidian),
+                'g',
+                new ItemStack(Block.blockGold));
+
+        // Add record crafting.
+        GameRegistry.addRecipe(new ItemStack(records.get("fire")),
+                " f ",
+                "frf",
+                " f ",
+                'r',
+                new ItemStack(blankObsidianRecord),
+                'f',
+                new ItemStack(Item.fireballCharge));
+        
+        GameRegistry.addRecipe(new ItemStack(records.get("pirate")),
+                " s ",
+                "fof",
+                " b ",
+                'r',
+                new ItemStack(blankObsidianRecord),
+                's',
+                new ItemStack(Item.swordSteel),
+                'f',
+                new ItemStack(Item.fishRaw),
+                'b',
+                new ItemStack(Item.boat));
+    }
+
+    /**
+     * Helper to set up string localizations.
+     * 
+     * @return nothing
+     * 
+     * @author Neer Sighted
+     */
+    public static void setupLocalizations() {
+        // Name the items.
+        // They all share the same class, so just name one record.
         LanguageRegistry.addName(records.get("fire"), "Obsidian Disc");
         LanguageRegistry
                 .addName(blankObsidianRecord, "Unencoded Obsidian Disc");
@@ -160,13 +254,5 @@ public class PlayRecords {
         // Name the creative tab.
         LanguageRegistry.instance().addStringLocalization("itemGroup.tabDiscs",
                 "Music Discs");
-
-        // Set up renderers.
-        proxy.registerRenderers();
-    }
-
-    @PostInit
-    public void postInit(FMLPostInitializationEvent event) {
-        // stub
     }
 }
